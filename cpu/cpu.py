65bb96212f42110718b34b9088a00a0e26e73d49
AlexeyKurakov <alexeykurakov5565@gmail.com>
2025-04-10 15:40:08 +0300

    added realtime

--- cpu/cpu.py
+++ cpu/cpu.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, jsonify, Response
+import random
 import psutil
-import time
-import datetime
 import threading
+import time
+from flask import Flask, render_template
+from turbo_flask import Turbo
+
+app = Flask(__name__)
+turbo = Turbo(app)
 
 def get_bar(percent):
     progress = round(percent * 0.1 + 0.5)
-    return '|' + '█' * progress + '_' * (10 - progress) + '|'
+    return '|' + '██' * progress + '__' * (10 - progress) + '|'
 
 def get_CPU_report():
     return 'cpu {} {}%'.format(
@@ -20,16 +24,25 @@ def get_RAM_report():
         psutil.virtual_memory().percent
     )
 
-app = Flask(__name__)
+@app.context_processor
+def inject_load():
+    load = [int(random.random() * 100) / 100 for _ in range(3)]
+    return {'cpu': get_CPU_report(), 'ram': get_RAM_report()}
 
-@app.route('/report', methods=['GET','POST'])
-def report():
-    return render_template(
-        'index.html', 
-        cpu_report= get_CPU_report(), 
-        ram_report= get_RAM_report(), 
-        date = str(datetime.datetime.now())
-    )
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(1)
+            turbo.push(turbo.replace(render_template('index.html'), 'load'))
+            inject_load()
+
+th = threading.Thread(target=update_load)
+th.daemon = True
+th.start()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port= 5000, debug=True)
